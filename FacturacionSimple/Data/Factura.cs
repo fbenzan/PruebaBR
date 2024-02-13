cfa29f3678ce47eb3b09e6e09b3d090856c76ca4
Felix Benzan <139989847+fbenzan@users.noreply.github.com>
2024-02-13 19:53:24 -0400

    Se crean las clases para los datos de las facturas

---
+++ FacturacionSimple/Data/Factura.cs
@@ -0,0 +1,9 @@
+﻿namespace FacturacionSimple.Data;
+
+public class Factura
+{
+    public int Numero { get; set; }
+    public string Cliente { get; set; }
+    public DateTime Fecha { get; set; }
+    public List<FacturaDetalle> Detalles { get; set; }
+}
---
+++ FacturacionSimple/Data/FacturaDetalle.cs
@@ -0,0 +1,10 @@
+﻿namespace FacturacionSimple.Data;
+
+public class FacturaDetalle
+{
+    public int Cantidad { get; set; }
+    public string Producto { get; set; }
+    public decimal PrecioVenta { get; set; }
+
+    public decimal SubTotal => Cantidad * PrecioVenta;
+}
