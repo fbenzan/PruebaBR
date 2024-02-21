702ba336eb74266f3302dbba896d513bd50a216b
Felix Benzan <139989847+fbenzan@users.noreply.github.com>
2024-02-20 21:06:40 -0400

    Cambios en Facturas Services

--- FacturacionSimple/Servicios/FacturaServices.cs
+++ FacturacionSimple/Servicios/FacturaServices.cs
@@ -1,22 +1,31 @@
-﻿using FacturacionSimple.Data;
+﻿using FacturacionSimple.Contexto;
+using FacturacionSimple.Data;
 
 namespace FacturacionSimple.Servicios;
 
 public class FacturaServices : IFacturaServices
 {
+    private readonly IFSDbContext dbContext;
+
+    public FacturaServices(IFSDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
     //Metodo para consultar las facturas..
-    public List<Factura> Get(string filtro) => Memoria
-        .Facturas.Where(f => f.Cliente.Contains(filtro)).ToList();
+    public List<Factura> Get(string filtro) => 
+        dbContext.Facturas.Where(f => f.Cliente.Contains(filtro)).ToList();
+    //Memoria.Facturas.Where(f => f.Cliente.Contains(filtro)).ToList();
     //Metodo para guardar la factura...
     public bool Create(Factura data)
     {
         try
         {
-            var numero = (Memoria.Facturas != null && Memoria.Facturas.Any()) ?
-            Memoria.Facturas.Max(f => f.Numero) + 1 : 1;
+            var numero = (dbContext.Facturas.Any()) ?
+            dbContext.Facturas.Max(f => f.Numero) + 1 : 1;
             data.Numero = numero;
             data.Fecha = DateTime.Now;
-            Memoria.Facturas?.Add(data);
+            dbContext.Facturas.Add(data);
+            dbContext.SaveChanges();
             return true;
         }
         catch
