0e98c2d048105c5d51382d8bc420f62dbcd1359e
Felix Benzan <139989847+fbenzan@users.noreply.github.com>
2024-02-20 20:40:43 -0400

    Preparando las entidades para el manejo de base de datos

--- FacturacionSimple/Data/Factura.cs
+++ FacturacionSimple/Data/Factura.cs
@@ -1,19 +1,29 @@
-﻿namespace FacturacionSimple.Data;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FacturacionSimple.Data;
 
 public class Factura
 {
+
+    [Key]
+    public int Id { get; set; }
     public int Numero { get; set; }
     public string Cliente { get; set; } = "";
     public DateTime Fecha { get; set; }
-    public List<FacturaDetalle> Detalles { get; set; }
-    = new();
+    public ICollection<FacturaDetalle> Detalles { get; set; } = new HashSet<FacturaDetalle>();
 
+    [NotMapped]
     public int Unidades =>
         (Detalles != null && Detalles.Any()) ? 
         Detalles.Sum(d => d.Cantidad) : 0;
+
+    [NotMapped]
     public decimal Descuento =>
         (Detalles != null && Detalles.Any()) ?
         Detalles.Sum(d => d.Descuento) : 0;
+
+    [NotMapped]
     public decimal Total =>
         (Detalles != null && Detalles.Any()) ?
         Detalles.Sum(d => d.SubTotal) : 0;
--- FacturacionSimple/Data/FacturaDetalle.cs
+++ FacturacionSimple/Data/FacturaDetalle.cs
@@ -1,12 +1,23 @@
-﻿namespace FacturacionSimple.Data;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FacturacionSimple.Data;
 
 public class FacturaDetalle
 {
+    [Key]
+    public int IdDetalle { get; set; }
+    public int FacturaId { get; set; }
     public int Cantidad { get; set; }
     public string Producto { get; set; } = "";
     public decimal PrecioVenta { get; set; }
     public float PorcentajeDescuento { get; set; }//0-100
+    [NotMapped]
     private decimal ValorDescuento => (((decimal)PorcentajeDescuento / 100)* PrecioVenta);
+    [NotMapped]
     public decimal Descuento => (ValorDescuento * Cantidad);
+    [NotMapped]
     public decimal SubTotal => (Cantidad * PrecioVenta)-Descuento;
+    [ForeignKey(nameof(FacturaId))]
+    public virtual Factura? Factura { get; set; }
 }
