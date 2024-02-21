a82af1f73fa898920be0c38b33f6a8fc5cc57a6e
Felix Benzan <139989847+fbenzan@users.noreply.github.com>
2024-02-20 20:41:01 -0400

    Creando la clase DbContext para la conexion a base de datos

---
+++ FacturacionSimple/Contexto/FSDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FacturacionSimple.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FacturacionSimple.Contexto;
+
+public class FSDbContext : DbContext
+{
+    #region Tablas de mi base de datos
+    public DbSet<Factura> Facturas { get; set; }
+    public DbSet<FacturaDetalle> FacturasDetalle { get; set; }
+    #endregion
+    #region La configuracion de conexion a mi DB.
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=FSDbContext.db");
+    }
+    #endregion
+}
